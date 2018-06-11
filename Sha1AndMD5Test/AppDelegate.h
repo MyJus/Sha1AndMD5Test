f2c5d6feb742e9e2aefcfbc0672b5688b9f82554
刘俊君 <peony@tiancangcang.local>
2018-06-11 09:39:37 +0800

    Initial Commit

---
+++ Sha1AndMD5Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sha1AndMD5Test
+//
+//  Created by peony on 2018/6/11.
+//  Copyright © 2018年 peony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sha1AndMD5Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sha1AndMD5Test
+//
+//  Created by peony on 2018/6/11.
+//  Copyright © 2018年 peony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
